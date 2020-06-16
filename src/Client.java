d35911b544a4c46435593e175b5c2995716eac1c
Paul-Cristian Vasile <paul.cristian.vasile04@gail.com>
2020-06-16 13:09:50 +0300

    Updated client with register

--- src/Client.java
+++ src/Client.java
@@ -20,32 +20,54 @@ public class Client {
             myReader.close();
             Client.loop(session_data);
         } catch (FileNotFoundException e) {
-            //The User is not logged in - try login
-            Scanner scanner = new Scanner( System.in);
-            System.out.println("=============================");
-            System.out.println("username:");
-            String username = scanner.nextLine();
-            System.out.println("password:");
-            String password = scanner.nextLine();
-            System.out.println("=============================");
-            String response = ServerManager.SendRequest("LOGIN " + username + " " + password);
-            String[] tokens = response.split(" ");
-            if(tokens[0].equals("OK")){
-                //Create session file
-                File session = new File("session.txt");
-                session.createNewFile();
-                FileWriter myWriter = new FileWriter("session.txt");
-                myWriter.write(tokens[1]);
-                myWriter.close();
-                Client.loop(tokens[1]);
-            }else{
-                System.out.println(response);
-                return;
+            //Promt the user to login or to create account
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Type 1 for login | 2 for register");
+            Integer option = scanner.nextInt();
+
+            if (option == 1) {
+                //The User is not logged in - try login
+                System.out.println("=============================");
+                System.out.println("username:");
+                String username = scanner.next();
+                System.out.println("password:");
+                String password = scanner.next();
+                System.out.println("=============================");
+                String response = ServerManager.SendRequest("LOGIN " + username + " " + password);
+                String[] tokens = response.split(" ");
+                if (tokens[0].equals("OK")) {
+                    //Create session file
+                    File session = new File("session.txt");
+                    session.createNewFile();
+                    FileWriter myWriter = new FileWriter("session.txt");
+                    myWriter.write(tokens[1]);
+                    myWriter.close();
+                    Client.loop(tokens[1]);
+                } else {
+                    System.out.println(response);
+                    return;
+                }
+            } else {
+                //Register the user
+                System.out.println("Choose a name:");
+                String username = scanner.next();
+                System.out.println("Choose a password:");
+                String password = scanner.next();
+                String response = ServerManager.SendRequest("CREATE_ACCOUNT " + username + " " + password);
+                String[] t = response.split(" ");
+                if (t[0].equals("OK")) {
+                    System.out.println("Account created succesfully - please login");
+                    return;
+                } else {
+                    System.out.println("Error creating an account");
+                    return;
+                }
             }
         }
 
     }
-    public static void loop(String session){
+
+    public static void loop(String session) {
 
         System.out.println("Logged in with session id: " + session);
 
@@ -53,15 +75,15 @@ public class Client {
         new Thread(() -> {
             String response = ServerManager.SendRequest("READ_MAILBOX " + session);
             String[] tokens = response.split(" ");
-            if(tokens[0].equals("OK")){
+            if (tokens[0].equals("OK")) {
                 total_email_count = tokens.length - 1;
-            }else{
+            } else {
                 System.out.println("Errors getting emails");
                 return;
             }
         }).start();
 
-        while(true){
+        while (true) {
 
             System.out.println("Emails: " + total_email_count);
 
@@ -72,7 +94,7 @@ public class Client {
             String[] tokens = command.split(" ");
             String type = tokens[0];
 
-            switch(type) {
+            switch (type) {
                 case "LOGOUT":
                     //Delete local session file
                     File f = new File("session.txt");
@@ -80,9 +102,9 @@ public class Client {
                     //Send request to server
                     String response = ServerManager.SendRequest("LOGOUT " + session);
                     String[] t = response.split(" ");
-                    if(t[0].equals("OK")){
+                    if (t[0].equals("OK")) {
                         System.out.println("Logged out");
-                    }else{
+                    } else {
                         System.out.println("Error logging out");
                     }
                     return;
@@ -90,9 +112,9 @@ public class Client {
                     //Send request to server
                     String response2 = ServerManager.SendRequest(command + " " + session);
                     String[] t2 = response2.split(" ");
-                    if(t2[0].equals("OK")){
+                    if (t2[0].equals("OK")) {
                         System.out.println("Message sent");
-                    }else{
+                    } else {
                         System.out.println("Error sending message");
                     }
                     break;
@@ -100,9 +122,9 @@ public class Client {
                     //Send request to server
                     String response3 = ServerManager.SendRequest(command + " " + session);
                     String[] t3 = response3.split(" ");
-                    if(t3[0].equals("OK")){
+                    if (t3[0].equals("OK")) {
                         System.out.println(response3);
-                    }else{
+                    } else {
                         System.out.println("Error reading mailbox");
                     }
                     break;
@@ -110,9 +132,9 @@ public class Client {
                     //Send request to server
                     String response4 = ServerManager.SendRequest(command + " " + session);
                     String[] t4 = response4.split(" ");
-                    if(t4[0].equals("OK")){
+                    if (t4[0].equals("OK")) {
                         System.out.println(response4);
-                    }else{
+                    } else {
                         System.out.println("Error reading message");
                     }
                     break;
